5c12d4b61ea6ba3c6ad9f056b4254b47aff794fd
Marlon Silva <marlon.m_silva@outlook.com>
2024-12-20 22:45:00 -0300

    homepage animations

--- app/page.jsx
+++ app/page.jsx
@@ -1,44 +1,62 @@
 import Photo from "@/components/Photo";
 import Social from "@/components/Social";
 import { Button } from "@/components/ui/button";
-import { FiDownload } from 'react-icons/fi';
+import { FiDownload } from "react-icons/fi";
 
 const Home = () => {
+  const name = ["M", "a", "r", "l", "o", "n", " "," ", "S", "i", "l", "v", "a"];
+
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left">
+          {/* Texto */}
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Full Stack Developer</span>
             <h1 className="h1 mb-6">
-              Hello I'm <br/> <span className="text-sky-500">Marlon Silva</span>
-              </h1>
-            <p className="max-w-[500px] mb-9 text-white/89">
-            Text
-            </p>
-           
-           <div className="flex flex-col xl:flex-row items-center gap-8"> 
-           <Button
-           varian="outtline"
-           size="lg"
-           className="uppercase flex items-center gap-2"
-           >
-              <span>Donwload CV</span>
-              <FiDownload className="text-xl"/> 
-            </Button>
-            <div className="mb-8 xl:mb-0">
-              <Social containersStyle="flex gap-6" iconStyle="w-9 h-9 border border-customColor border-2 rounded-full flex justify-center items-center text-customColor text-base hover:bg-customColor hover:text-primary hover:transition-all duration-500"/>
+              Hello I'm <br />
+              <span className="inline-block mt-10">
+                {name.map((char, index) => (
+                  <span
+                    key={index}
+                    className={`inline-block text-customColor transition-all duration-300 hover:text-6xl ${
+                      char === " " ? "mx-1" : ""
+                    }`}
+                  >
+                    {char}
+                  </span>
+                ))}
+              </span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/89">Text</p>
+
+            {/* Botão e Redes Sociais */}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center border-2 rounded-lg border-customColor text-customColor p-2 gap-2 hover:text-lg duration-1000"
+              >
+                <span >Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social
+                  containersStyle="flex gap-6"
+                  iconStyle="w-9 h-9 border border-customColor border-2 rounded-full flex justify-center items-center text-customColor text-base hover:bg-customColor hover:text-primary hover:w-10 hover:h-10 hover:transition-all duration-500"
+                />
+              </div>
             </div>
-           </div>
-           
-           </div>
-          <div>
+          </div>
+
+          {/* Imagem */}
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
           </div>
         </div>
       </div>
     </section>
   );
-}
+};
 
-export default Home
+export default Home;
--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,29 +1,34 @@
 "use client";
 
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 import Image from "next/image";
 
 const Photo = () => {
   return (
     <div className="w-full h-full relative">
-        <motion.div>
-            <div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px]">
-                <div className="absolute inset-0 rounded-b-full  border-transparent flex items-end justify-center overflow-hidden  rounded-t-full bg-gradient-to-b from-slate-950 to-sky-900 
-                shadow-2xl ">
-                    
-                    <Image
-                        src="/assets/marlon.png"
-                        priority
-                        quality={100}
-                        fill
-                        alt=""
-                        className="object-cover opacity-85 shadow-2xl"
-                    />
-                </div>
-            </div>
-        </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 2, duration: 0.01, ease: "easeIn" },
+        }}
+      >
+        <div className="relative group w-[298px] h-[298px] xl:w-[498px] xl:h-[498px]">
+          {/* Imagem */}
+          <div className="absolute inset-0 rounded-full border-0 border-transparent bg-gradient-to-b from-slate-950 to-sky-900 shadow-2xl overflow-hidden hover:border-2 hover:border-customColor transition-all duration-200">
+            <Image
+              src="/assets/marlon.png"
+              priority
+              quality={100}
+              fill
+              alt=""
+              className="object-cover opacity-85 shadow-2xl"
+            />
+          </div>
+        </div>
+      </motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default Photo
+export default Photo;
